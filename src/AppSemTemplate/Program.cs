5cb4e202cb51876c9571179249c7d7f505dafe7a
= <=>
2024-03-06 08:56:34 -0300

    Atualizações - Identity - 06030900

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,10 +1,19 @@
 using AppSemTemplate.Data;
+using AppSemTemplate.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews();
+
+//SEGURANÇA CONTRA ATAQUES
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+});
 
 // Adicionando suporte a mudança de convenção da rota das areas.
 builder.Services.Configure<RazorViewEngineOptions>(options =>
@@ -17,14 +26,46 @@ builder.Services.Configure<RazorViewEngineOptions>(options =>
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+builder.Services.AddTransient<IOperacaoTransient, Operacao>();
+builder.Services.AddScoped<IOperacaoScoped, Operacao>();
+builder.Services.AddSingleton<IOperacaoSingleton, Operacao>();
+builder.Services.AddSingleton<IOperacaoSingletonInstance>(new Operacao(Guid.Empty));
+
+builder.Services.AddTransient<OperacaoService>();
+
 builder.Services.AddDbContext<AppDbContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+    options.ExcludedHosts.Add("example.com");
+    options.ExcludedHosts.Add("www.example.com");
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+}).AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+
+}
+else
+{
+    app.UseHsts();
+}
 
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthorization();
 
 // Rota de área genérica (não necessário se usar especializada)
 //app.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
@@ -36,5 +77,16 @@ app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Gestao}/{
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+
+    var singService = services.GetRequiredService<IOperacaoSingleton>();
+
+    Console.WriteLine("Direto da Program.cs" + singService.OperacaoId);
+}
 
 app.Run();
